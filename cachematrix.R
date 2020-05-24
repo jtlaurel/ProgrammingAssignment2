a2775104801194011ecd185d89da612d22f41e9d
jtlaurel <jtlaurel46@gmail.com>
2020-05-24 00:11:00 -0700

    Commit of makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 ## cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
